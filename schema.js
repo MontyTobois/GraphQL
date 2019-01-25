4f321cf288895f5b8c36cedf77aca22d89c53a73
Andre-Tobois <tobois.andre@gmail.com>
2019-01-25 16:07:03 -0500

    feat: Setup hardcoded data to run in GraphiQL.

--- schema.js
+++ schema.js
@@ -7,6 +7,44 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({
+// Hardcoded Data
+const customers = [
+  {id:'1', name: 'John Doe', email: 'jdoe@gmail.com', age:30},
+  {id:'2', name: 'Sherry Smith', email: 'ssmith@gmail.com', age:45},
+  {id:'3', name: 'Alex Doe', email: 'adoe@gmail.com', age:25}
+]
 
-})
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name:'Customer',
+  fields:() => ({
+      id: {type:GraphQLString},
+      name: {type: GraphQLString},
+      email: {type: GraphQLString},
+      age: {type: GraphQLInt},
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+      customer: {
+        type: CustomerType,
+        args: {
+          id: {type:GraphQLString}
+        },
+        resolve(parentValue, args){
+          for(let i = 0;i < customers.length;i++){
+            if(customers[i].id == args.id){
+              return customers[i];
+            }
+          }
+        }
+      }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
